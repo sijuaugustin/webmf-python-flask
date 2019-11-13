12d7ee6761f979f64b83eee6368b47d2e283ed53
sijukaugustin <naithan&kin7>
2019-11-13 17:56:02 +0530

    testing build

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/hello')  # this route is not working
 @app.route('/hello/')
 def hello_world():
-    return 'Hello worldfdfdddg!\n'
+    return 'Hello worldfdfdds!\n'
 
 @app.route('/hello/<username>') # dynamic route
 def hello_user(username):
